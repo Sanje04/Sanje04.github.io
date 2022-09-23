f3412705782a133158e7589e95879effe053a54e
Sanje Divakaran <sanje.divakaran@gmail.com>
2022-09-23 16:53:18 -0400

    updates to the code and assets

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './footer.css';
+
+const Footer = () => {
+  console.log("test")
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import ReactDOM from "react";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from './App';
 import "./index.css"
+const container = document.getElementById('root');
 
-ReactDOM.render(<App/>, document.querySelector("#root"));
+// Create a root.
+const root = ReactDOM.createRoot(container);
+
+
+root.render(<App/>);
 
