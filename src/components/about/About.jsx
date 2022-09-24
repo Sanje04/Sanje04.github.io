fb09a0ebddc7fc721ab838e8b403ab724d1bab6b
Sanje Divakaran <sanje.divakaran@gmail.com>
2022-09-24 16:42:59 -0400

    progress made to the about page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
 import './About.css';
+import ME from '../../assets/me.png';
+import { FaAward, FaFolder } from 'react-icons/fa';
+import { HiUsers } from 'react-icons/hi';
 
 const about = () => {
   return (
-    <div>about</div>
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="" />
+          </div>
+        </div>
+
+        <div className="about__container">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='aboutIcon' />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+          </div>
+
+          <div className="about__cards">
+            <article className='about__card'>
+              <HiUsers className='aboutIcon' />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+          </div>
+
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaFolder className='aboutIcon' />
+              <h5>Projects</h5>
+              <small>80+</small>
+            </article>
+          </div>
+
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './nav.css'
+import { AiOutlineHome, AiOutlineUser, AiOutlineBook, AiOutlineCustomerService, AiOutlineContacts } from 'react-icons/ai';
+import { useState } from 'react';
+
 
 const Nav = () => {
+
+  const [activeNav, setActiveNav] = useState('#');
+
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === "#" ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === "#about" ? 'active' : ''}><AiOutlineUser /></a>
+      <a href="#experiences" onClick={() => setActiveNav('#experiences')} className={activeNav === "#experiences" ? 'active' : ''}><AiOutlineBook /></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === "#services" ? 'active' : ''}><AiOutlineCustomerService /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === "#contact" ? 'active' : ''}><AiOutlineContacts /></a>
+    </nav>
   )
 }
 
