a8abe36111761e72085768a155801eb8698f4bb4
Sanje Divakaran <sanje.divakaran@gmail.com>
2022-09-25 02:16:06 -0400

    portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,77 @@
-import React from 'react'
-import './portfolio.css'
+import React from 'react';
+import './portfolio.css';
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'test 1',
+    github: 'https://github.com'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'test 2',
+    github: 'https://github.com'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'test 3',
+    github: 'https://github.com'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'test 4',
+    github: 'https://github.com'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'test 5',
+    github: 'https://github.com'
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'test 6',
+    github: 'https://github.com'
+  },
+]
+
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      
+
+      <div className="container portfolio__container">
+       {
+        data.map(({id, image, title, github}) => {
+          return (
+            <article key={id} className='portfolio__item'>
+            <div className="portfolio__item-image">
+              <img src={image} alt={title} />
+              <h3>{title}</h3>
+              <div className='portfolio__item-cta'>
+                <a href={github} className='btn'>Github</a>
+                
+              </div>
+            </div>
+          </article>
+          )
+        })
+       }
+      </div>
+    </section>
   )
 }
 
