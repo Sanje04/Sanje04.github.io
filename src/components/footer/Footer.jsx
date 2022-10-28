a8a67d497788df099edcf8e401ba0db8bf48f9ca
Sanje Divakaran <sanje.divakaran@gmail.com>
2022-10-28 14:50:27 -0400

    fixed with the edits

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
 
   return (
     <footer>
-      <a href="#" className='footer__logo'>Sanje Divakaran</a>
+      <a href="/#" className='footer__logo'>Sanje Divakaran</a>
 
       <ul className='permalinks'>
-        <li><a href="#">Home</a></li>
+        <li><a href="!#">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#skills">Skills</a></li>
